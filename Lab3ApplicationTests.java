545f20761929a68b7fc1a62e4f9bcbda06f4a21f
ReenaRaikar <155934362+ReenaRaikar@users.noreply.github.com>
2024-02-04 21:47:58 -0800

    Add files via upload

---
+++ Lab3ApplicationTests.java
@@ -0,0 +1,16 @@
+package mes.casetools.lab3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+		sum1 obj=new sum1();
+		int answer=obj.sum(1,2,3)
+	
+	}
+
+}
